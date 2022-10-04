705c1e3f3fb9cf4ef0a71ba2b4d26020a9a8effc
Emil Kenguerli <emil.kenguerli@thedelta.io>
2022-10-04 17:11:17 +0200

    Feat: Follower data formatted

--- data_formatter.py
+++ data_formatter.py
@@ -5,6 +5,7 @@ pd.options.mode.chained_assignment = None
 
 df = pd.read_csv('final.csv')
 df1 = pd.read_csv('result.csv')
+df2 = pd.read_csv('followers.csv')
 
 exclude_list = ['director', 'assistant', 'instructor', 'actor']
 excludes_df = df1.loc[~df1['title'].str.contains('|'.join(exclude_list), case=False, na=False)]
@@ -12,12 +13,17 @@ excludes_df = df1.loc[~df1['title'].str.contains('|'.join(exclude_list), case=Fa
 contains_cto = excludes_df.loc[excludes_df['title'].str.contains('cto', case=False, na=False)]
 contains_cto['cto'] = True
 
-final = pd.merge(df, contains_cto[['companyName', 'cto']],on='companyName', how='left')
+final = pd.merge(df, contains_cto[['companyName', 'cto', 'fullName']],on='companyName', how='left')
+final['Person name'] = final['fullName']
+
+final = final.assign(result=final['Person name'].isin(df2['fullName']).astype(int))
+final.loc[final['result'] == 1, 'Delta LI Follower'] = True
+final.loc[final['result'] == 0, 'Delta LI Follower'] = False
 
 final['Do they have a CTO on LI'] = final['cto']
 final.loc[final['cto'] != True, 'Do they have a CTO on LI'] = False
 
-final.drop(['cto'], axis=1, inplace=True)
+final.drop(['cto', 'fullName', 'result'], axis=1, inplace=True)
 
 final.to_csv('yeboi.csv')
 
