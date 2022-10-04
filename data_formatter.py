817787717dad5393f17494982850a4caeaf509a0
Emil Kenguerli <emil.kenguerli@thedelta.io>
2022-10-04 21:16:45 +0200

    Feat: Added remaining fields for cto

--- data_formatter.py
+++ data_formatter.py
@@ -12,9 +12,15 @@ excludes_df = df1.loc[~df1['title'].str.contains('|'.join(exclude_list), case=Fa
 
 contains_cto = excludes_df.loc[excludes_df['title'].str.contains('cto', case=False, na=False)]
 contains_cto['cto'] = True
+contains_cto['cofounder'] = False
 
-final = pd.merge(df, contains_cto[['companyName', 'cto', 'fullName']],on='companyName', how='left')
+contains_cto.loc[contains_cto['title'].str.contains('founder', case=False, na=False) == True, 'cofounder'] = True
+
+final = pd.merge(df, contains_cto[['companyName', 'cto', 'fullName', 'title', 'cofounder']],on='companyName', how='left')
+
 final['Person name'] = final['fullName']
+final['Role (LI)'] = final['title']
+final['Also Cofounder?'] = final['cofounder']
 
 final = final.assign(result=final['Person name'].isin(df2['fullName']).astype(int))
 final.loc[final['result'] == 1, 'Delta LI Follower'] = True
@@ -23,7 +29,6 @@ final.loc[final['result'] == 0, 'Delta LI Follower'] = False
 final['Do they have a CTO on LI'] = final['cto']
 final.loc[final['cto'] != True, 'Do they have a CTO on LI'] = False
 
-final.drop(['cto', 'fullName', 'result'], axis=1, inplace=True)
+final.drop(['cto', 'fullName', 'result', 'title'], axis=1, inplace=True)
 
 final.to_csv('yeboi.csv')
-
